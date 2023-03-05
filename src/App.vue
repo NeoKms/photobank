c91df23074a7b0b345dd742dbd83092bb8bc77b2
Владислав Жигульский <upachko@gmail.com>
2023-03-05 17:51:04 +0300

    add title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import {useI18n} from "vue-i18n";
+import {computed, watch} from "vue";
+
+const i18n = useI18n();
+const title = computed(()=>i18n.t("title"));
+watch(title,()=>document.title = title.value);
 </script>
 
 <template>
